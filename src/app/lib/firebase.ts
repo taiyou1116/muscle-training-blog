e5c80601a6063d58c5608ea4b2b24160f6130e38
taiyou <hiromacha1116@icloud.com>
2023-11-06 20:23:29 +0900

    auth関数をfirebaseに

--- src/app/lib/firebase.ts
+++ src/app/lib/firebase.ts
@@ -1,7 +1,7 @@
 
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { doc, getDoc, getFirestore, setDoc } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_APIKEY,
@@ -21,3 +21,38 @@ const provider = new GoogleAuthProvider();
 const db = getFirestore(app);
 
 export { auth, provider, db };
+
+
+// Googleでサインイン & DBと紐付け
+export const signInWithGoogle = async () => {
+  const result = await signInWithPopup(auth, provider);
+
+  // 認証情報からユーザー情報を取得
+  const user = result.user;
+
+  const userRef = doc(db, "posts", user.uid);
+  const docSnap = await getDoc(userRef);
+
+  // ユーザーデータがまだFirestoreにない場合は新しく作成
+  if (!docSnap.exists()) {
+    await setDoc(userRef, {
+      uid: user.uid,
+      email: user.email,
+      displayName: user.displayName,
+      photoURL: user.photoURL,
+      // その他保存したいユーザー情報
+    });
+    console.log("新しいユーザープロファイルを作成しました。");
+  } else {
+    console.log("既存のユーザーです。");
+  }
+}
+
+export const signOut = () => {
+  auth.signOut();
+}
+
+export const showUserProfile = () => {
+  const photoURL = auth.currentUser?.photoURL;
+  return photoURL;
+}
--- src/app/ui/auth.tsx
+++ src/app/ui/auth.tsx
@@ -1,32 +1,6 @@
-import { signInWithPopup } from "firebase/auth";
-import { auth, db, provider } from "../lib/firebase"
+import { showUserProfile, signInWithGoogle, signOut } from "../lib/firebase"
 import Image from "next/image";
-import { doc, getDoc, setDoc } from "firebase/firestore";
 import React from 'react';
-
-const signInWithGoogle = async () => {
-  const result = await signInWithPopup(auth, provider);
-
-  // 認証情報からユーザー情報を取得
-  const user = result.user;
-
-  const userRef = doc(db, "posts", user.uid);
-  const docSnap = await getDoc(userRef);
-
-  // ユーザーデータがまだFirestoreにない場合は新しく作成
-  if (!docSnap.exists()) {
-    await setDoc(userRef, {
-      uid: user.uid,
-      email: user.email,
-      displayName: user.displayName,
-      photoURL: user.photoURL,
-      // その他保存したいユーザー情報
-    });
-    console.log("新しいユーザープロファイルを作成しました。");
-  } else {
-    console.log("既存のユーザーです。");
-  }
-}
 
 // サインインボタン
 export function SignInButton() {
@@ -47,7 +21,7 @@ export function SignOutButton() {
     <div>
       <button 
         className=" bg-red-400 p-2 rounded-md text-white font-bold shadow-md hover:bg-red-500" 
-        onClick={() => auth.signOut()}>
+        onClick={() => signOut()}>
         サインアウト
       </button>
     </div>
@@ -56,7 +30,7 @@ export function SignOutButton() {
 
 // ユーザー情報の表示
 export function UserInfo() {
-  const photoURL = auth.currentUser?.photoURL;
+  const photoURL = showUserProfile();
 
   return (
     <div>
