d61edb62fbd2f91752b5d6909365d1ed45e597ac
taiyou <hiromacha1116@icloud.com>
2023-11-09 14:45:59 +0900

    UI途中

--- src/app/ui/post.tsx
+++ src/app/ui/post.tsx
@@ -12,36 +12,39 @@ function Post(props: Props) {
   const { data } = props;
 
   return (
-    <div className='bg-slate-200 w-full flex flex-col p-5 gap-3 rounded-md border border-slate-400 cursor-pointer'>
-      <div className=' flex gap-3  bg-slate-300 p-3 rounded-lg shadow-lg'>
+    <div className='bg-white rounded-lg shadow p-6'>
+      <div className=' flex items-center mb-6'>
         {data.parentData.photoURL ? (
           <Image
             src={data.parentData.photoURL}
             alt="User profile picture" // alt属性
-            width={25} // 画像の幅
-            height={25} // 画像の高さ
+            width={50} // 画像の幅
+            height={50} // 画像の高さ
+            className='h-10 w-10 rounded-full'
           />
         ) : (
           <p>No profile picture available</p>
         )}
-        { data.parentData.displayName }
+        <div className="ml-4">
+          <p className="text-lg font-bold text-gray-800">{data.parentData.displayName}</p>
+        </div>
       </div>
-      <div className=' bg-slate-50 p-3'>
+      <div className=''>
         { data.exercisesData.map((d: ExerciseData, dataIndex: number) => (
-        <React.Fragment key={dataIndex}>
-          <h1 className=' font-semibold'>{d.selectedExercise}</h1>
+        <div key={dataIndex} className="mb-4">
+          <h2 className="text-xl font-semibold text-gray-900 mb-2">{d.selectedExercise}</h2>
           {d.sets.map((set, index) => (
-            <div key={index}>
+            <div key={index} className="flex items-center space-x-2">
               <span>{ index + 1 }セット目: </span>
               <span>Reps: {set.reps} </span>
               <span>Weight: {set.weight}</span>
             </div>
           ))}
-        </React.Fragment>
+        </div>
       )) }
       </div>
       
-      <div className=' p-3'>
+      <div className="mt-6 text-gray-700">
         { data.text }
       </div>
     </div>
--- src/app/ui/time-line.tsx
+++ src/app/ui/time-line.tsx
@@ -7,7 +7,7 @@ export default async function TimeLine() {
   const res = await getData();
 
   return (
-      <div className='flex flex-col items-center w-3/5 h-full bg-slate-50 rounded-md gap-8 p-10'>
+      <div className='flex flex-col items-center w-3/5 h-[calc(100vh-7rem)] bg-slate-200 rounded-md gap-8 px-10 py-5 overflow-auto'>
        { res.map((data, index) => (
          <div key={index} className='w-full'>
           <Post 
